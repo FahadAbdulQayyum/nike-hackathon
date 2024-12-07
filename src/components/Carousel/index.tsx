a4faa46c26c0e1e2a2eb1429578713508be4264b
Fahad <fahad.45854@iqra.edu.com>
2024-12-08 00:09:48 +0500

    Images in flash sales with add to cart buttons

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { useState, useEffect } from "react"
+import { CiHeart } from 'react-icons/ci'
+import { RiEyeLine } from "react-icons/ri";
 
 import { Card, CardContent } from "@/components/ui/card"
 import {
@@ -12,6 +14,8 @@ import {
     type CarouselApi
 } from "@/components/ui/carousel"
 import Image from "next/image"
+import { BiStar } from "react-icons/bi"
+import { BsStarFill } from "react-icons/bs"
 
 export function CarouselImage({ flash, data }: any) {
     const [api, setApi] = useState<CarouselApi>()
@@ -97,17 +101,39 @@ export function CarouselImage({ flash, data }: any) {
                                     className="flex items-center justify-center text-black"
                                 >
                                     <span
-                                        className="flex flex-col justify-between"
+                                        className="flex flex-col justify-between font-500"
                                     >
-                                        <span className="bg-primaryy h-48 w-full flex items-center justify-center">
+                                        {/* <span className="bg-primaryy h-48 w-full flex items-center justify-center relative">
+                                            <span className="bg-tertiary absolute top-2 left-2 text-white rounded p-1 px-2 text-sm">{_.discount}</span>
+                                            <span className="bg-white absolute top-2 right-2 text-black rounded- p-1 px-2 text-xl"><CiHeart /></span>
+                                            <span className="bg-white absolute top-[50px] right-2 text-black rounded- p-1 px-2 text-xl"><RiEyeLine /></span>
                                             <Image src={_.image} alt={_.title} width={100} height={100} className="z-10" />
+                                            <button className="bg-black absolute bottom-0 left-0 right-0 text-white rounded- p-1 px-2 text-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300">Add to Cart</button>
+                                        </span> */}
+
+                                        <span className="bg-primaryy h-48 w-full flex items-center justify-center relative group">
+                                            <span className="bg-tertiary absolute top-2 left-2 text-white rounded p-1 px-2 text-sm">{_.discount}</span>
+                                            <span className="bg-white absolute top-2 right-2 text-black rounded p-1 px-2 text-xl"><CiHeart /></span>
+                                            <span className="bg-white absolute top-[50px] right-2 text-black rounded p-1 px-2 text-xl"><RiEyeLine /></span>
+                                            <Image src={_.image} alt={_.title} width={100} height={100} className="z-10" />
+                                            <button className="bg-black absolute bottom-0 left-0 right-0 text-white p-1 px-2 text-[15px] opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                                Add to Cart
+                                            </button>
                                         </span>
+
                                         <span>{_.title}</span>
                                         <span>
-                                            <span>{_.price}</span>
+                                            <span className="text-tertiary mr-5">{_.price}</span>
                                             <span className="">{_.originalPrice}</span>
                                         </span>
-                                        <span>{_.starCounts}</span>
+                                        <span
+                                            className="flex"
+                                        >
+                                            {/* <span>{+_.starCounts * 5}</span> */}
+                                            {/* <span><BiStar /></span> */}
+                                            <span className="flex">{Array.from({ length: _.starCounts }).map(v => <><BsStarFill color="orange" /></>)}</span>
+                                            <span className="px-2">({_.point})</span>
+                                        </span>
                                     </span>
                                 </CardContent>
                                 {/* </Card> */}
